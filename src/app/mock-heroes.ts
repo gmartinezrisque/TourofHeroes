462c2734fc2e693e42471a2861d7e4c7b5cfb2f1
Gus <gmarris@DESPOSUME>
2020-04-23 15:46:56 +0200

    3. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Courtouis'},
+	{ id: 12, name: 'Carvajal'},
+	{ id: 13, name: 'Mendi'},
+	{ id: 14, name: 'Sergio Ramos'},
+	{ id: 15, name: 'Varane'},
+	{ id: 16, name: 'Cassemiro'},
+	{ id: 17, name: 'Modric'},
+	{ id: 18, name: 'Kross'},
+	{ id: 19, name: 'Bale'},
+	{ id: 20, name: 'Benzema'}
+]
